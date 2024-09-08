6f10743df4e99b4226ac55cb5446fdc0123d7b56
dphu2609 <leducphu.login@gmail.com>
2024-09-08 11:28:34 +0700

    add more details to analytics

--- frontend/src/app/analytics/page.js
+++ frontend/src/app/analytics/page.js
@@ -1,12 +1,12 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import AnalyticsBarChart from "@/components/analytics/AnalyticsBarChart";
+import TimeChart from "@/components/analytics/AnalyticsBarChart";
 
 const AnalyticsPage = () => {
     return (
         <div className="flex items-center justify-center">
-            <AnalyticsBarChart />
+            <TimeChart />
         </div>
     );
 }
